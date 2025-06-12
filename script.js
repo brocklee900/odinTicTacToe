e02f90c54bf4eb65489c8cbf9e5d5768d24986ae
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-06-11 22:33:49 -0700

    Add game/player objects in an IIFE factory func

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
-console.log("woah");
+const game = (function () {
+    
+    //Create GameBoard
+    const gameBoard = (function () {
+    	let playingBoard = [
+        [-1, -1, -1],
+        [-1, -1, -1],
+        [-1, -1, -1]
+        ];
+        
+        const printBoard = () => console.log(playingBoard);
+        return {printBoard};
+	})();
+    
+    
+    //Create Players
+    function makePlayer(playerMarker) {
+    	const marker = playerMarker;
+        let score = 0;
+        
+        const printStatus = () => console.log(marker, score);
+        const addScore = () => score++;
+        return {printStatus, addScore};
+    };
+    
+    const playerOne = makePlayer("x");
+    const playerTwo = makePlayer("o");
+    
+    //Create GameManager
+    const gameManager = (function(gameBoard) {
+    	const printBoard = () => gameBoard.printBoard();
+        return {printBoard};
+    })(gameBoard);
+    
+    return { gameBoard, playerOne, playerTwo, gameManager };
 
+})();
+
+game.gameBoard.printBoard();
+game.playerOne.printStatus();
+game.playerOne.addScore();
+game.playerOne.printStatus();
+game.playerTwo.printStatus();
+game.playerTwo.addScore();
+game.playerTwo.addScore();
+game.playerTwo.printStatus();
+game.gameManager.printBoard();
