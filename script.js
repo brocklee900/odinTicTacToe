77287d46d06c4e5a72bdc2a24ef329f524685557
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-06-11 22:39:27 -0700

    Add functions to check for end of game/win/ties

--- script.js
+++ script.js
@@ -2,14 +2,60 @@ const game = (function () {
     
     //Create GameBoard
     const gameBoard = (function () {
-    	let playingBoard = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
+    	let playingBoard = ['', '', '', '', '', '', '', '', '']
         
         const printBoard = () => console.log(playingBoard);
+        
+        //Check board for tie game
+        const checkGameTie = () => {
+        	return playingBoard.includes('') ? false : true;
+        }
+        
+        //Check board to see if a player has won the game
+        const checkGameWin = () => {
+        	//Rows
+        	if (playingBoard[0] != '' && playingBoard[0] == playingBoard[1] && playingBoard[1] == playingBoard[2]) {
+            	return true;
+            } else if (playingBoard[3] != '' && playingBoard[3] == playingBoard[4] && playingBoard[4] == playingBoard[5]) {
+            	return true;
+            } else if (playingBoard[6] != '' && playingBoard[6] == playingBoard[7] && playingBoard[7] == playingBoard[8]) {
+            	return true;
+            //Columns
+            } else if (playingBoard[0] != '' && playingBoard[0] == playingBoard[3] && playingBoard[3] == playingBoard[6]) {
+            	return true;
+            } else if (playingBoard[1] != '' && playingBoard[1] == playingBoard[4] && playingBoard[4] == playingBoard[7]) {
+            	return true;
+            } else if (playingBoard[2] != '' && playingBoard[2] == playingBoard[5] && playingBoard[5] == playingBoard[8]) {
+            	return true;
+            } else if (playingBoard[0] != '' && playingBoard[0] == playingBoard[4] && playingBoard[4] == playingBoard[8]) {
+            	return true;
+			} else if (playingBoard[2] != '' && playingBoard[2] == playingBoard[4] && playingBoard[4] == playingBoard[6]) {
+            	return true;
+			} else {
+            	return false;
+            }
+        }
+        
+        //Check if the Game has ended
+        //1 if a player won, 0 if the game is in progress, -1 if its a tie
+        const checkGameEnd = () => {
+        	if (checkGameWin()) {
+            	return 1;
+            } else if (checkGameTie()) {
+            	return -1;
+            } else {
+            	return 0;
+            }
+        }
+        
         //Update Board when player makes a move
         const updateBoard = (marker, position) => {
-        	playingBoard[position] = marker;
+        	if (playingBoard[position] == '') {
+            	playingBoard[position] = marker;
+            }
         }
-        return {printBoard, updateBoard};
+        
+        return {printBoard, updateBoard, checkGameEnd};
 	})();
     
     
@@ -49,6 +95,7 @@ const game = (function () {
         const playerAction = (position) => {
         	gameBoard.updateBoard(currentPlayer.getMarker(), position);
             gameBoard.printBoard();
+            console.log(gameBoard.checkGameEnd());
             changeTurn();
         }
         return {playerAction};
@@ -57,7 +104,12 @@ const game = (function () {
     return { gameManager };
 })();
 
+game.gameManager.playerAction(0);
+game.gameManager.playerAction(2);
 game.gameManager.playerAction(1);
+game.gameManager.playerAction(3);
 game.gameManager.playerAction(5);
 game.gameManager.playerAction(4);
+game.gameManager.playerAction(6);
 game.gameManager.playerAction(8);
+game.gameManager.playerAction(7);
